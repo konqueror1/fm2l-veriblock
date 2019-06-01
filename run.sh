8a8c02037941b0c1c4320aa9dca384532ad9dffa
Trustfarm-heart <trustfarm.info@gmail.com>
2019-06-01 17:33:17 +0900

    updates ubuntu run.sh and multi port support binary

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ do
   # ./veri_fm2l -o pool.veriblockpool.com:8501 -u V6hhHVMxdcRzfT2rNFr1kmVq86riun  -p FM2C ;
   #  ./veri_fm2l -o dabpool.com:8501 -u V6hhHVMxdcRzfT2rNFr1kmVq86riun  -p FM2C ;
   #  ./veri_fm2l -o vbk-fpga.luckypool.io:9501 -u V6hhHVMxdcRzfT2rNFr1kmVq86riun -p FM2C ;
-    ./veri_fm2l -o vbk.luckypool.io:9501 -u V6hhHVMxdcRzfT2rNFr1kmVq86riun -p FM2C ;
+    ./veri_fm2l -o vbk.luckypool.io:9501 -u V6hhHVMxdcRzfT2rNFr1kmVq86riun -p FM2C -nl /dev/ttyUSB0,/dev/ttyUSB1 ;
    # ./veri_fm2l -o vbk.luckypool.io:9501 -u V8wB8DP56Cs9VrwyrXVsLTQ3e5Ngj5 -p FM2C -v true;
    # ./veri_fm2l -o vbk.luckypool.io:9501 -u V8wB8DP56Cs9VrwyrXVsLTQ3e5Ngj5 -p FM2C -v false;
    # ./veri_fm2l -o vbk-fpga.luckypool.io:9501 -u V6hhHVMxdcRzfT2rNFr1kmVq86riun -p FM2C ;
