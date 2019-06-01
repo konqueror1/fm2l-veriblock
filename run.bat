6a6a0ef18f4763b0cb1bb4c11fc8d6a076259587
trustfarm-vr1 <trustfarm.info@gmail.com>
2019-06-01 13:57:42 +0900

    modify batchfile and shell scripts for fm2L

--- run.bat
+++ run.bat
@@ -1,15 +1,15 @@
 @echo off
-SET APP_TITLE=VeriBlock NVidia GPU Miner
+SET APP_TITLE=VeriBlock FPGA FM2L Miner
 TITLE %APP_TITLE%
 
 :Execute
 
-VeriBlock-NodeCore-PoW-CUDA -o testnet-pool-gpu.veriblock.org:8501 -u YOUR_ADDRESS_HERE -d 0 -l false
-
+ veri_fm2l -o vbk.luckypool.io:9501 -u V6hhHVMxdcRzfT2rNFr1kmVq86riun -p FM2C
+ 
 echo ----------------------------------------------------------------------
-echo ^| GPU Miner is restarting in 10 seconds!                             ^|
+echo ^| FPGA FM2L Miner is restarting in 1 seconds!                             ^|
 echo ^| Press CTRL+C to cancel this or ENTER to proceed immediately...     ^|
 echo ----------------------------------------------------------------------
-timeout /t 10 > NUL
-echo Restarting GPU Miner...
+timeout /t 1 > NUL
+echo Restarting %%APP_TITLE%%...
 goto Execute
--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 while true;
 do
-#    ./nodecore_pow_cuda -o pool.veriblockpool.com:8501 -u V8wB8DP56Cs9VrwyrXVsLTQ3e5Ngj5 -p FM2c;
-  # ./veri_amd -o pool.veriblockpool.com:8501 -u V6hhHVMxdcRzfT2rNFr1kmVq86riun  -p FM2C ;
-  #  ./veri_amd -o dabpool.com:8501 -u V6hhHVMxdcRzfT2rNFr1kmVq86riun  -p FM2C ;
-  #  ./veri_amd -o vbk-fpga.luckypool.io:9501 -u V6hhHVMxdcRzfT2rNFr1kmVq86riun -p FM2C ;
-    ./veri_amd -o vbk.luckypool.io:9501 -u V6hhHVMxdcRzfT2rNFr1kmVq86riun -p FM2C ;
-   # ./veri_amd -o vbk.luckypool.io:9501 -u V8wB8DP56Cs9VrwyrXVsLTQ3e5Ngj5 -p FM2C -v true;
-   # ./veri_amd -o vbk.luckypool.io:9501 -u V8wB8DP56Cs9VrwyrXVsLTQ3e5Ngj5 -p FM2C -v false;
-   # ./veri_amd -o vbk-fpga.luckypool.io:9501 -u V6hhHVMxdcRzfT2rNFr1kmVq86riun -p FM2C ;
-#     ./veri_amd -o vbk-us.luckypool.io:9501 -u V6hhHVMxdcRzfT2rNFr1kmVq86riun -p FM2C -l flase ;
-   # ./veri_amd -o vbk.luckypool.io:9501 -u V8wB8DP56Cs9VrwyrXVsLTQ3e5Ngj5 -p FM2C ;
-  echo "Restarting PoW miner!";
+#    
+  # ./veri_fm2l -o pool.veriblockpool.com:8501 -u V6hhHVMxdcRzfT2rNFr1kmVq86riun  -p FM2C ;
+  #  ./veri_fm2l -o dabpool.com:8501 -u V6hhHVMxdcRzfT2rNFr1kmVq86riun  -p FM2C ;
+  #  ./veri_fm2l -o vbk-fpga.luckypool.io:9501 -u V6hhHVMxdcRzfT2rNFr1kmVq86riun -p FM2C ;
+    ./veri_fm2l -o vbk.luckypool.io:9501 -u V6hhHVMxdcRzfT2rNFr1kmVq86riun -p FM2C ;
+   # ./veri_fm2l -o vbk.luckypool.io:9501 -u V8wB8DP56Cs9VrwyrXVsLTQ3e5Ngj5 -p FM2C -v true;
+   # ./veri_fm2l -o vbk.luckypool.io:9501 -u V8wB8DP56Cs9VrwyrXVsLTQ3e5Ngj5 -p FM2C -v false;
+   # ./veri_fm2l -o vbk-fpga.luckypool.io:9501 -u V6hhHVMxdcRzfT2rNFr1kmVq86riun -p FM2C ;
+#     ./veri_fm2l -o vbk-us.luckypool.io:9501 -u V6hhHVMxdcRzfT2rNFr1kmVq86riun -p FM2C -l flase ;
+   # ./veri_fm2l -o vbk.luckypool.io:9501 -u V8wB8DP56Cs9VrwyrXVsLTQ3e5Ngj5 -p FM2C ;
+  echo "Restarting veri_FM2L miner!";
   sleep 1;
 done;
 
