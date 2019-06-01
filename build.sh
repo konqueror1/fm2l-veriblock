274cbbbeb92d587d86dde3bca4dd43f046b951c0
trustfarm-vr1 <trustfarm.info@gmail.com>
2019-06-01 12:42:00 +0900

    delete unusable files

--- build.sh
+++
@@ -1,2 +0,0 @@
-/usr/local/cuda/bin/nvcc  -gencode=arch=compute_52,code=\"sm_52,compute_52\" -I/usr/local/cuda/include -I. -O3 -Xcompiler -Wall  -D_FORCE_INLINES  --ptxas-options="-v" --maxrregcount=64 -o nodecore_pow_cuda kernel.cu main.cpp Miner.cpp -Xcompiler -static-libgcc -Xcompiler -static-libstdc++ -std=c++11
-
